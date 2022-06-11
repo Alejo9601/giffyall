3f959ac07edaed771336fbf12a7b07243de445da
Alejo9601 <alejandro96jm@gmail.com>
2022-06-11 11:07:34 -0300

    Feat, styled SearchBar component

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,10 +8,11 @@ const HeaderSection = styled.section`
 `;
 const HomeTitle = styled.h1`
   color: #00b7a8;
-  font-size: 3rem;
+  font-size: 5rem;
   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
   font-style: italic;
   text-shadow: -3px 3px black;
+  margin-top: 30px;
 `;
 
 const Header = () => {
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,39 @@
+import styled from "styled-components";
+
+const Form = styled.form`
+  display: flex;
+  justify-content: center;
+  background-color: white;
+  margin-top: 20px;
+  border-radius: 3px;
+  overflow: hidden;
+`;
+const StyledSearchBar = styled.input`
+  width: 290px;
+  height: 50px;
+  font-size: 1.2rem;
+  text-align: center;
+  border: 0;
+  outline: none;
+`;
+const StyledButton = styled.input`
+  background-color: #34b3f1;
+  color: white;
+  height: 50px;
+  border: 0;
+  padding: 5px;
+`;
+
 const SearchBar = () => {
   return (
-    <form>
-      <input></input>
-    </form>
+    <Form>
+      <StyledSearchBar
+        type="search"
+        name="search"
+        placeholder="make a gif search here..."
+      />
+      <StyledButton type="submit" name="submit" value="SEARCH" />
+    </Form>
   );
 };
 
