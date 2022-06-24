f113abeda65974523f939d690bd76ee9f49a9dd0
Alejo9601 <alejandro96jm@gmail.com>
2022-06-23 23:53:19 -0300

    Fixed, gifs title provokes overflow

--- src/components/GifsTitle.js
+++ src/components/GifsTitle.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
 const Title = styled.h1`
-  width: 100%;
-  text-align: left;
+  max-width: fit-content;
+  text-align: center;
   color: white;
   font-size: 2.5rem;
-  padding: 0px 0px 0px 30px;
+  padding: 0px 0px 0px 15px;
   margin-bottom: 20px;
   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
 
