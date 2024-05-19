d32c08eb1ab6298023a1425b549b677bc218a401
Alejo9601 <alejandro96jm@gmail.com>
2024-05-19 18:30:51 -0300

    Created layout

---
+++ src/layouts/CommonWrapper.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const ResultsSection = styled.section`
+   display: grid;
+
+   @media (min-width: 1366px) {
+      grid-template-columns: 1.5fr 3fr;
+      align-self: center;
+   }
+
+   @media (min-width: 1600px) {
+      grid-template-columns: 1fr 3fr;
+      align-self: center;
+   }
+`;
+
+export function CommonWrapper({ children }) {
+   return <ResultsSection>{children}</ResultsSection>;
+}
