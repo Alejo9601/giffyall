ec739707d24b5274171f80fe03c0e957785fc1ef
Alejo9601 <alejandro96jm@gmail.com>
2024-05-23 15:59:22 -0300

    Added component login to App routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import styled from "styled-components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchResults from "./pages/SearchResults";
@@ -7,27 +8,28 @@ import Header from "./components/Header";
 import { GifsContextProvider } from "./context/GifsContext";
 
 const GifApp = styled.div`
-  min-width: 100vw;
-  display: flex;
-  flex-direction: column;
+   min-width: 100vw;
+   display: flex;
+   flex-direction: column;
 `;
 
 function App() {
-  return (
-    <BrowserRouter>
-      <GifsContextProvider>
-        <GifApp>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/search/:term" element={<SearchResults />} />
-            <Route path="/detail/:gifId" element={<GifDetail />} />
-          </Routes>
-        </GifApp>
-      </GifsContextProvider>
-    </BrowserRouter>
-  );
+   return (
+      <BrowserRouter>
+         <GifsContextProvider>
+            <GifApp>
+               <Header />
+               <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/home" element={<Home />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/search/:term" element={<SearchResults />} />
+                  <Route path="/detail/:gifId" element={<GifDetail />} />
+               </Routes>
+            </GifApp>
+         </GifsContextProvider>
+      </BrowserRouter>
+   );
 }
 
 export default App;
