a690be1ab5ebac88965bdb14784318a6025ddc1f
Alejo9601 <alejandro96jm@gmail.com>
2024-05-23 15:23:58 -0300

    Added Login component

---
+++ src/pages/Login.js
@@ -0,0 +1,107 @@
+import { useUser } from "../hooks/useUser";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
+
+const StyledSection = styled.section`
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   gap: 5px;
+   background-color: 000;
+   height: 100%;
+   width: 100%;
+`;
+
+const FormContainer = styled.div`
+   display: flex;
+   background-color: white;
+   border-radius: 8px;
+`;
+
+const StyledHeading = styled.h1`
+   color: white;
+   font-size: 2rem;
+`;
+
+const StyledForm = styled.form`
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   margin: 15px;
+   width: 300px;
+   height: 300px;
+`;
+
+const StyledInput = styled.input`
+   width: 100%;
+   height: 40px;
+   padding: 10px 0px 10px 0px;
+   border-radius: 8px;
+   padding: 0px 20px 0px 20px;
+   background-color: beige;
+`;
+
+const SytledLabel = styled.label`
+   width: 100%;
+   margin-top: 8px;
+`;
+
+const StyledSubmitButton = styled.input`
+   color: white;
+   margin-top: 30px;
+   padding: 10px 20px 10px 20px;
+   align-self: flex-end;
+   background-color: blueviolet;
+   cursor: pointer;
+   font-size: 1rem;
+   border-radius: 4px;
+`;
+
+const Login = () => {
+   const { login } = useUser();
+   const passwordRef = useRef();
+   const usernameRef = useRef();
+   const navigation = useNavigate();
+
+   async function handleLogin(ev) {
+      ev.preventDefault();
+
+      if (login(usernameRef.current.value, passwordRef.current.value)) {
+         navigation(`/home`);
+      }
+   }
+
+   return (
+      <StyledSection>
+         <StyledHeading />
+         <FormContainer>
+            <StyledForm onSubmit={handleLogin}>
+               <SytledLabel htmlFor="username">
+                  Username
+                  <StyledInput
+                     ref={usernameRef}
+                     type="text"
+                     name="username"
+                     placeholder="alejo9601"
+                  />
+               </SytledLabel>
+               <SytledLabel htmlFor="password">
+                  Password
+                  <StyledInput
+                     ref={passwordRef}
+                     type="password"
+                     name="password"
+                     placeholder="***********"
+                  />
+               </SytledLabel>
+               <StyledSubmitButton type="submit" value="Login" />
+            </StyledForm>
+         </FormContainer>
+      </StyledSection>
+   );
+};
+
+export default Login;
