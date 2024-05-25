20088aab96333435ddae0ed99939a9613eadc229
Alejo9601 <alejandro96jm@gmail.com>
2024-05-25 15:31:35 -0300

    Added component

---
+++ src/components/FormButton.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const StyledFormButton = styled.input`
+   color: white;
+   margin-top: 30px;
+   padding: 10px 20px 10px 20px;
+   align-self: flex-end;
+   background-color: blueviolet;
+   cursor: pointer;
+   font-size: 1rem;
+   border-radius: 4px;
+`;
+
+export default function FormButton({ textValue, onClick }) {
+   return (
+      <StyledFormButton
+         type="button"
+         value={textValue}
+         onClick={() => onClick()}
+      />
+   );
+}
