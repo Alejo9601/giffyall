400a63f425220544a39ac5af47367337aa7c245e
Alejo9601 <alejandro96jm@gmail.com>
2024-05-29 11:06:10 -0300

    UserContext now gets sessionStorage property

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,10 +1,14 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 
 export const UserContext = createContext("");
 
 export function UserContextProvider({ children }) {
    const [userName, setUsername] = useState("Not Logged");
    const [userToken, setUserToken] = useState("");
+
+   useEffect(() => {
+      setUsername(sessionStorage.getItem("userLogged"));
+   }, []);
 
    const data = {
       userName,
