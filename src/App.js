88ad9778d9fca8da77ac5c83c594f5b8de07af98
Alejo9601 <alejandro96jm@gmail.com>
2024-05-28 21:36:14 -0300

    Added UserContext to App

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SearchResults from "./pages/SearchResults";
 import GifDetail from "./pages/GifDetail";
 import { GifsContextProvider } from "./context/GifsContext";
 import SignUpForm from "./pages/SignUpForm";
+import { UserContextProvider } from "./context/UserContext";
 
 const GifApp = styled.div`
    min-width: 100vw;
@@ -15,20 +16,22 @@ const GifApp = styled.div`
 
 function App() {
    return (
-      <BrowserRouter>
-         <GifsContextProvider>
-            <GifApp>
-               <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/home" element={<Home />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/signup" element={<SignUpForm />} />
-                  <Route path="/search/:term" element={<SearchResults />} />
-                  <Route path="/detail/:gifId" element={<GifDetail />} />
-               </Routes>
-            </GifApp>
-         </GifsContextProvider>
-      </BrowserRouter>
+      <UserContextProvider>
+         <BrowserRouter>
+            <GifsContextProvider>
+               <GifApp>
+                  <Routes>
+                     <Route path="/" element={<Home />} />
+                     <Route path="/home" element={<Home />} />
+                     <Route path="/login" element={<Login />} />
+                     <Route path="/signup" element={<SignUpForm />} />
+                     <Route path="/search/:term" element={<SearchResults />} />
+                     <Route path="/detail/:gifId" element={<GifDetail />} />
+                  </Routes>
+               </GifApp>
+            </GifsContextProvider>
+         </BrowserRouter>
+      </UserContextProvider>
    );
 }
 
