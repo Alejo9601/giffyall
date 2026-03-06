68678efd77d83a5d55aed0a3e65fbe959d253ef6
Alejo9601 <alejandro96jm@gmail.com>
2026-03-05 22:43:28 -0300

    fix : implemented .env variables

--- src/services/consumeGifs.js
+++ src/services/consumeGifs.js
@@ -1,35 +1,35 @@
 const getGifs = (termToSearch = null, osffsetParam) => {
-  const baseUrl = "https://api.giphy.com/v1/gifs";
-  const searchEndpoint = "/search";
-  const trendingEndpoint = "/trending";
-  const apiKey = "?api_key=FlQhEsM9VzKUZb1Zx4XT2IRUTygAEOs2";
-  const toSearch = `&q=${termToSearch}`;
-  const limit = "&limit=25";
-  const offset = `&offset=${osffsetParam}`;
-  const rating = "&rating=g";
-  const lang = "&lang=en";
+   const baseUrl = process.env.REACT_APP_BASE_URL;
+   const searchEndpoint = "/search";
+   const trendingEndpoint = "/trending";
+   const apiKey = process.env.REACT_APP_API_KEY;
+   const toSearch = `&q=${termToSearch}`;
+   const limit = "&limit=25";
+   const offset = `&offset=${osffsetParam}`;
+   const rating = "&rating=g";
+   const lang = "&lang=en";
 
-  const url =
-    baseUrl +
-    `${termToSearch !== null ? searchEndpoint : trendingEndpoint}` +
-    apiKey +
-    toSearch +
-    limit +
-    offset +
-    rating +
-    lang;
+   const url =
+      baseUrl +
+      `${termToSearch !== null ? searchEndpoint : trendingEndpoint}` +
+      apiKey +
+      toSearch +
+      limit +
+      offset +
+      rating +
+      lang;
 
-  return fetch(url)
-    .then((res) => {
-      return res.ok
-        ? res.json()
-        : Promise.reject({
-            err: true,
-            status: res.status || "00",
-            statusText: res.statusText || "Ocurrio un error",
-          });
-    })
-    .catch((err) => err);
+   return fetch(url)
+      .then((res) => {
+         return res.ok
+            ? res.json()
+            : Promise.reject({
+                 err: true,
+                 status: res.status || "00",
+                 statusText: res.statusText || "Ocurrio un error",
+              });
+      })
+      .catch((err) => err);
 };
 
 export default getGifs;
